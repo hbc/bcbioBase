34cfb4cfdd5a6af3434f590781909511051eddea
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-11 12:07:04 -0500

    Reexport basejump functions used in bcbioRNASeq

--- R/reexports.R
+++ R/reexports.R
@@ -1,10 +1,50 @@
+#' @importFrom basejump annotable
+#' @export
+basejump::annotable
+
+#' @importFrom basejump camel
+#' @export
+basejump::camel
+
+#' @importFrom basejump checkGene2symbol
+#' @export
+basejump::checkGene2symbol
+
+#' @importFrom basejump detectOrganism
+#' @export
+basejump::detectOrganism
+
+#' @importFrom basejump fixNA
+#' @export
+basejump::fixNA
+
+#' @importFrom basejump gene2symbol
+#' @export
+basejump::gene2symbol
+
+#' @importFrom basejump plotHeatmap
+#' @export
+basejump::plotHeatmap
+
 #' @importFrom basejump readYAML
 #' @export
 basejump::readYAML
 
+#' @importFrom basejump sanitizeAnnotable
+#' @export
+basejump::sanitizeAnnotable
+
+#' @importFrom basejump snake
+#' @export
+basejump::snake
+
+
+
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
+
+
 
 #' @importFrom S4Vectors DataFrame
 #' @export
@@ -13,6 +53,8 @@ S4Vectors::DataFrame
 #' @importFrom S4Vectors metadata
 #' @export
 S4Vectors::metadata
+
+
 
 #' @importFrom tibble glimpse
 #' @export
