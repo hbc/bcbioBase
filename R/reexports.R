e0ab0ed2912d5dba69fd5b3171470a4c523e90d2
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-11 10:36:09 -0500

    Fix NAMESPACE for unit tests

--- R/reexports.R
+++ R/reexports.R
@@ -5,3 +5,15 @@ basejump::readYAML
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
+
+#' @importFrom S4Vectors DataFrame
+#' @export
+S4Vectors::DataFrame
+
+#' @importFrom S4Vectors metadata
+#' @export
+S4Vectors::metadata
+
+#' @importFrom tibble glimpse
+#' @export
+tibble::glimpse
