21930075a7221f9570769d148da57faf6c7976ec
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-25 19:24:09 -0500

    Sunset tibble support

--- R/methods-prepareSummarizedExperiment.R
+++ R/methods-prepareSummarizedExperiment.R
@@ -174,11 +174,6 @@ NULL
         abort("colData must support `dim()`")
     }
     colData <- as.data.frame(colData)
-    # Handle tibble rownames
-    if (!has_rownames(colData) &
-        "rowname" %in% colnames(colData)) {
-        colData <- column_to_rownames(colData)
-    }
     if (!has_rownames(colData)) {
         abort("colData missing rownames")
     }
