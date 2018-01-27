3987ea4ae3f7df729114bc9cd0de9f767c0b6b65
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-27 09:08:28 -0500

    Don't save `prepareTemplate()` output

--- docs/reference/setup.R
+++ docs/reference/setup.R
@@ -1,19 +1,24 @@
-library(basejump)
+suppressPackageStartupMessages(library(bcbioSingleCell))
 library(knitr)
-library(ggplot2)
 library(tidyverse)
+
+# Set seed for reproducibility
+set.seed(1454944673L)
 
 opts_chunk[["set"]](
     audodep = TRUE,
     cache = TRUE,
     cache.lazy = FALSE,
-    error = FALSE,
+    error = TRUE,
     fig.height = 10L,
     fig.retina = 2L,
     fig.width = 10L,
     message = FALSE,
     tidy = TRUE,
-    warning = FALSE)
+    warning = TRUE)
 
-theme_set(theme_gray(base_size = 14L))
-theme_update(legend.position = "bottom")
+theme_set(
+    theme_light(base_size = 14L))
+theme_update(
+    legend.justification = "center",
+    legend.position = "bottom")
