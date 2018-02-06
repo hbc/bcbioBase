6073dd0e68b6aadcd6c6928060ba6b721af48718
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-06 17:12:31 -0500

    Version bump to v0.0.4 and initial commit of `copyToDropbox()`

---
+++ R/copyToDropbox.R
@@ -0,0 +1,48 @@
+#' Copy Files to Dropbox
+#'
+#' @author Michael Steinbaugh, Victor Barerra, John Hutchinson
+#'
+#' @importFrom rdrop2 drop_auth drop_delete drop_exists drop_share drop_upload
+#'
+#' @param files Local file paths.
+#' @param dir Relative path of remote Dropbox directory.
+#'
+#' @return Invisibly return [list] of rdrop2 output.
+#' @export
+copyToDropbox <- function(files, dir) {
+    if (!(is.character(files) || is.list(files))) {
+        abort("`files` must be a character vector or list")
+    }
+    if (!is_string(dir)) {
+        abort("`dir` must be a string")
+    }
+
+    # Check that local files exist
+    if (!all(vapply(files, file.exists, logical(1L)))) {
+        missing <- !vapply(files, file.exists, logical(1L))
+        abort(paste(
+            "Missing local files:",
+            toString(basename(files[missing]))
+        ))
+    }
+
+    # Ensure user is authenticated with Dropbox
+    drop_auth()
+
+    # Loop across the files in list
+    rdrop <- lapply(files, function(file) {
+        dropboxFile <- file.path(dir, basename(file))
+        # Delete file if it exists already. Otherwise `drop_share()`
+        # is currently erroring out when we try to re-share an existing
+        # file.
+        if (drop_exists(dropboxFile)) {
+            drop_delete(dropboxFile)
+        }
+        drop_upload(file = file, path = dir)
+        drop_share(
+            path = dropboxFile,
+            requested_visibility = "public")
+    })
+
+    invisible(rdrop)
+}
