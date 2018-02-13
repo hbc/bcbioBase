4d77c442df045589a74e758e2438687ebca2ec13
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-13 12:06:13 -0500

    Add back rlang imports

--- R/bcbioBase-package.R
+++ R/bcbioBase-package.R
@@ -3,6 +3,7 @@
 #' Base functions and generics for bcbio R packages.
 #'
 #' @import methods
+#' @importFrom rlang !!! !! .data abort inform sym syms warn
 "_PACKAGE"
 
 #' @importFrom utils globalVariables
