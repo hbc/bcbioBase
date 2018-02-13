f90a9226be88cdb1330c83520872c22496df08a9
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-13 15:10:45 -0500

    Update unit tests

--- tests/testthat/test_readSampleMetadataFile.R
+++ tests/testthat/test_readSampleMetadataFile.R
@@ -4,46 +4,33 @@ test_that("Demultiplexed FASTQ", {
     file <- file.path(
         "http://bcbiobase.seq.cloud",
         "sample_metadata",
-        "demultiplexed.xlsx"
-    )
+        "demultiplexed.xlsx")
     meta <- readSampleMetadataFile(file, quiet = TRUE)
 
     # Check that names are sanitized correctly
-    rows <- c("sample_1",
-              "sample_2",
-              "sample_3",
-              "sample_4")
-    expect_identical(
-        as.character(meta[["sampleID"]]),
-        rows
-    )
-    expect_identical(
-        rownames(meta),
-        rows
-    )
+    rows <- c("sample_1", "sample_2", "sample_3", "sample_4")
+    expect_identical(as.character(meta[["sampleID"]]), rows)
+    expect_identical(rownames(meta), rows)
 
     # Check that column names get set correctly
     expect_identical(
         colnames(meta),
-        c("sampleID",
-          "sampleName",
-          "description",
-          "fileName",
-          "genotype")
+        c("sampleID", "sampleName", "description", "fileName", "genotype")
     )
 
     # Lane-split technical replicate support
     meta <- readSampleMetadataFile(file, lanes = 4L, quiet = TRUE)
     expect_identical(
         rownames(meta)[1L:8L],
-        c("sample_1_L001",
-          "sample_1_L002",
-          "sample_1_L003",
-          "sample_1_L004",
-          "sample_2_L001",
-          "sample_2_L002",
-          "sample_2_L003",
-          "sample_2_L004")
+        c(
+            "sample_1_L001",
+            "sample_1_L002",
+            "sample_1_L003",
+            "sample_1_L004",
+            "sample_2_L001",
+            "sample_2_L002",
+            "sample_2_L003",
+            "sample_2_L004")
     )
     expect_identical(
         meta[1L, metadataPriorityCols],
@@ -66,12 +53,13 @@ test_that("Demultiplexed FASTQ", {
             file.path(
                 "http://bcbiobase.seq.cloud",
                 "sample_metadata",
-                "demultiplexed_with_sampleName.csv"
-            ),
-            quiet = TRUE
-        ),
-        paste("Specify only `description` and omit `sampleName`",
-              "for demultiplexed FASTQ file metadata")
+                "demultiplexed_with_sampleName.csv"),
+            quiet = TRUE),
+        paste(
+            "are_disjoint_sets :",
+            "\"sampleName\" and colnames\\(data\\) have common elements:",
+            "sampleName."
+        )
     )
 
     # Required column check failure
@@ -80,23 +68,27 @@ test_that("Demultiplexed FASTQ", {
             file.path(
                 "http://bcbiobase.seq.cloud",
                 "sample_metadata",
-                "demultiplexed_missing_cols.csv"
-            ),
-            quiet = TRUE
-        ),
-        "Required columns: fileName, description"
+                "demultiplexed_missing_cols.csv"),
+            quiet = TRUE),
+        paste(
+            "is_subset :",
+            "The element 'description' in requiredCols is not in",
+            "colnames\\(data\\)."
+        )
     )
 
     # Duplicated description
     expect_error(
         readSampleMetadataFile(
-            file.path("http://bcbiobase.seq.cloud",
-                      "sample_metadata",
-                      "demultiplexed_duplicated_description.csv"
-            ),
-            quiet = TRUE
-        ),
-        "`description` column must be unique for demultiplexed files"
+            file.path(
+                "http://bcbiobase.seq.cloud",
+                "sample_metadata",
+                "demultiplexed_duplicated_description.csv"),
+            quiet = TRUE),
+        paste(
+            "has_no_duplicates :",
+            "data\\[\\[\"description\"\\]\\] has a duplicate at position 2."
+        )
     )
 })
 
@@ -104,53 +96,54 @@ test_that("Multiplexed FASTQ", {
     file <- file.path(
         "http://bcbiobase.seq.cloud",
         "sample_metadata",
-        "multiplexed.xlsx"
-    )
+        "multiplexed.xlsx")
     meta <- readSampleMetadataFile(file, quiet = TRUE)
 
     expect_identical(
         rownames(meta),
-        c("run_1_CAGTTATG",
-          "run_1_TTACCTCC",
-          "run_2_ATAGCCTT",
-          "run_2_CTTAATAG",
-          "run_2_TAAGGCTC",
-          "run_2_TCGCATAA",
-          "run_2_TCTTACGC")
+        c(
+            "run_1_CAGTTATG",
+            "run_1_TTACCTCC",
+            "run_2_ATAGCCTT",
+            "run_2_CTTAATAG",
+            "run_2_TAAGGCTC",
+            "run_2_TCGCATAA",
+            "run_2_TCTTACGC")
     )
 
     # Lane-split technical replicate support
     meta <- readSampleMetadataFile(file, lanes = 4L, quiet = TRUE)
     expect_identical(
         rownames(meta),
-        c("run_1_L001_CAGTTATG",
-          "run_1_L001_TTACCTCC",
-          "run_1_L002_CAGTTATG",
-          "run_1_L002_TTACCTCC",
-          "run_1_L003_CAGTTATG",
-          "run_1_L003_TTACCTCC",
-          "run_1_L004_CAGTTATG",
-          "run_1_L004_TTACCTCC",
-          "run_2_L001_ATAGCCTT",
-          "run_2_L001_CTTAATAG",
-          "run_2_L001_TAAGGCTC",
-          "run_2_L001_TCGCATAA",
-          "run_2_L001_TCTTACGC",
-          "run_2_L002_ATAGCCTT",
-          "run_2_L002_CTTAATAG",
-          "run_2_L002_TAAGGCTC",
-          "run_2_L002_TCGCATAA",
-          "run_2_L002_TCTTACGC",
-          "run_2_L003_ATAGCCTT",
-          "run_2_L003_CTTAATAG",
-          "run_2_L003_TAAGGCTC",
-          "run_2_L003_TCGCATAA",
-          "run_2_L003_TCTTACGC",
-          "run_2_L004_ATAGCCTT",
-          "run_2_L004_CTTAATAG",
-          "run_2_L004_TAAGGCTC",
-          "run_2_L004_TCGCATAA",
-          "run_2_L004_TCTTACGC")
+        c(
+            "run_1_L001_CAGTTATG",
+            "run_1_L001_TTACCTCC",
+            "run_1_L002_CAGTTATG",
+            "run_1_L002_TTACCTCC",
+            "run_1_L003_CAGTTATG",
+            "run_1_L003_TTACCTCC",
+            "run_1_L004_CAGTTATG",
+            "run_1_L004_TTACCTCC",
+            "run_2_L001_ATAGCCTT",
+            "run_2_L001_CTTAATAG",
+            "run_2_L001_TAAGGCTC",
+            "run_2_L001_TCGCATAA",
+            "run_2_L001_TCTTACGC",
+            "run_2_L002_ATAGCCTT",
+            "run_2_L002_CTTAATAG",
+            "run_2_L002_TAAGGCTC",
+            "run_2_L002_TCGCATAA",
+            "run_2_L002_TCTTACGC",
+            "run_2_L003_ATAGCCTT",
+            "run_2_L003_CTTAATAG",
+            "run_2_L003_TAAGGCTC",
+            "run_2_L003_TCGCATAA",
+            "run_2_L003_TCTTACGC",
+            "run_2_L004_ATAGCCTT",
+            "run_2_L004_CTTAATAG",
+            "run_2_L004_TAAGGCTC",
+            "run_2_L004_TCGCATAA",
+            "run_2_L004_TCTTACGC")
     )
 
     # Required column check failure
@@ -159,23 +152,26 @@ test_that("Multiplexed FASTQ", {
             file.path(
                 "http://bcbiobase.seq.cloud",
                 "sample_metadata",
-                "multiplexed_missing_cols.csv"
-            ),
-            quiet = TRUE
-        ),
-        "Required columns: fileName, description, sampleName, index"
+                "multiplexed_missing_cols.csv"),
+            quiet = TRUE),
+        paste(
+            "is_subset :",
+            "The element 'index' in requiredCols is not in",
+            "colnames\\(data\\)."
+        )
     )
 
-    # Duplicated sampleName
+    # Duplicate rows in `sampleName` column
     expect_error(
         readSampleMetadataFile(
             file.path("http://bcbiobase.seq.cloud",
                       "sample_metadata",
-                      "multiplexed_duplicated_sampleName.csv"
-            ),
-            quiet = TRUE
-        ),
-        "`sampleName` column must be unique for multiplexed samples"
+                      "multiplexed_duplicated_sampleName.csv"),
+            quiet = TRUE),
+        paste(
+            "has_no_duplicates :",
+            "data\\[\\[\"sampleName\"\\]\\] has duplicates at positions 2, 4."
+        )
     )
 })
 
@@ -183,44 +179,48 @@ test_that("Legacy bcbio samplename column", {
     file <- file.path(
         "http://bcbiobase.seq.cloud",
         "sample_metadata",
-        "bcbio_legacy_samplename.csv"
-    )
-    meta <- suppressWarnings(
-        readSampleMetadataFile(file, quiet = TRUE)
-    )
+        "bcbio_legacy_samplename.csv")
+    meta <- suppressWarnings(readSampleMetadataFile(file, quiet = TRUE))
     expect_identical(
         meta,
         data.frame(
             # sanitized
-            sampleID = factor("sample_1",
-                              levels = "sample_1"),
+            sampleID = factor(
+                "sample_1",
+                levels = "sample_1"),
             # matches description
-            sampleName = factor("sample-1",
-                                levels = "sample-1"),
+            sampleName = factor(
+                "sample-1",
+                levels = "sample-1"),
             # unmodified
-            description = factor("sample-1",
-                                 levels = "sample-1"),
+            description = factor(
+                "sample-1",
+                levels = "sample-1"),
             # renamed `samplename`
-            fileName = factor("sample-1.fastq.gz",
-                              levels = "sample-1.fastq.gz"),
+            fileName = factor(
+                "sample-1.fastq.gz",
+                levels = "sample-1.fastq.gz"),
             # sanitized
             row.names = "sample_1"
         )
     )
     expect_warning(
         readSampleMetadataFile(file, quiet = TRUE),
-        "`samplename` is used in some bcbio examples for FASTQ file names"
+        "`samplename` \\(note case\\) is used in some bcbio examples"
     )
 })
 
-test_that("sampleID column already defined", {
+test_that("`sampleID` already defined by the user", {
     file <- file.path(
         "http://bcbiobase.seq.cloud",
         "sample_metadata",
-        "sampleID_column_defined.xlsx"
-    )
-    expect_warning(
+        "sampleID_column_defined.xlsx")
+    expect_error(
         readSampleMetadataFile(file, quiet = TRUE),
-        "`sampleID` should not be manually defined in the sample metadata file"
+        paste(
+            "are_disjoint_sets :",
+            "\"sampleID\" and colnames\\(data\\) have common elements:",
+            "sampleID."
+        )
     )
 })
