28e8e57a8de2c14e1efc46e406544ccb3eaa047e
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-13 14:17:52 -0500

    Update assertive imports

--- R/assert.R
+++ R/assert.R
@@ -5,9 +5,11 @@
 #' @keywords internal
 #'
 #' @importFrom assertive assert_all_are_existing_files
+#' @importFrom assertive assert_are_identical
 #' @importFrom assertive assert_are_intersecting_sets
 #' @importFrom assertive assert_has_colnames
 #' @importFrom assertive assert_has_dimnames
+#' @importFrom assertive assert_has_rownames
 #' @importFrom assertive assert_has_no_duplicates
 #' @importFrom assertive assert_is_a_bool
 #' @importFrom assertive assert_is_a_string
