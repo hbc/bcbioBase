9c784d28b4f14bb4808a0e7eec0dd466bed6dfb6
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-15 16:55:17 -0500

    Consolidate assert checks

--- R/assert.R
+++ R/assert.R
@@ -27,5 +27,39 @@
 #' @importFrom assertive assert_is_tbl
 #'
 #' @param severity How severe should the consequences of the assertion be?
-#'   Either `stop`, `warning`, or `message`.
+#'   Either "`stop`", "`warning`", "`message`", or "`none`".
 NULL
+
+
+
+#' Interesting Groups Formal Assert Check
+#'
+#' Prevent unwanted downstream behavior when a missing interesting group
+#' is requested by the user.
+#'
+#' @inherit assert
+#' @family Assert Checks
+#'
+#' @param object Object supporting [colnames()], typically a [data.frame].
+#' @param interestingGroups Interesting groups character vector.
+#'
+#' @export
+#'
+#' @examples
+#' demultiplexed <- file.path(
+#'     "http://bcbiobase.seq.cloud",
+#'     "sample_metadata",
+#'     "demultiplexed.xlsx")
+#' meta <- readSampleMetadataFile(demultiplexed)
+#' assert_formal_interesting_groups(meta, "genotype")
+assert_formal_interesting_groups <- function(
+    object,
+    interestingGroups,
+    severity = "stop") {
+    assert_has_colnames(object, severity = severity)
+    assert_is_subset(
+        x = interestingGroups,
+        y = colnames(object),
+        severity = severity
+    )
+}
