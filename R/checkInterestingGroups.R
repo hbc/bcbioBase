3f67f3b5e1e98fd9e967a8ef61d548a3bb70c7ac
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-18 08:43:43 -0500

    Add back `checkInterestingGroups()` and deprecate in a future release

---
+++ R/checkInterestingGroups.R
@@ -0,0 +1,48 @@
+# TODO Deprecate in favor of `assert_formal_interesting_groups`
+# nocov start
+
+
+
+#' Check Interesting Groups
+#'
+#' Prevent unwanted downstream behavior when a missing interesting group
+#' is requested by the user.
+#'
+#' @param object Object supporting [colnames()], typically a [data.frame].
+#' @param interestingGroups Interesting groups character vector.
+#' @param warnOnNULL Warn the user on `NULL` argument.
+#'
+#' @return Valid character of defined interesting groups. Stop on failure.
+#' @export
+#'
+#' @examples
+#' demultiplexed <- file.path(
+#'     "http://bcbiobase.seq.cloud",
+#'     "sample_metadata",
+#'     "demultiplexed.xlsx")
+#' meta <- readSampleMetadataFile(demultiplexed)
+#' checkInterestingGroups(object = meta, interestingGroups = "genotype")
+checkInterestingGroups <- function(
+    object,
+    interestingGroups,
+    warnOnNULL = FALSE) {
+    assert_has_colnames(object)
+    assert_is_subset(interestingGroups, colnames(object))
+
+    # Default to `sampleName` if `NULL`
+    if (is.null(interestingGroups)) {
+        if (isTRUE(warnOnNULL)) {
+            warn(paste(
+                "`interestingGroups` is NULL.",
+                "Defaulting to `sampleName`."
+            ))
+        }
+        interestingGroups <- "sampleName"
+    }
+
+    interestingGroups
+}
+
+
+
+# nocov end
--- R/deprecated.R
+++ R/deprecated.R
@@ -8,14 +8,3 @@
 #'
 #' @return No value.
 NULL
-
-
-
-# v0.1.1 =======================================================================
-#' @rdname deprecated
-#' @export
-checkInterestingGroups <- function() {
-    .Deprecated("assert_formal_interesting_groups")
-}
-
-# nocov end
