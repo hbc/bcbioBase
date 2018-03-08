fad4f8e000f38abf67ff24004e0fa5cd82f22aaf
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-08 11:10:47 -0500

    Add method support for SummarizedExperiment

---
+++ R/methods-interestingGroups.R
@@ -0,0 +1,54 @@
+#' Interesting Groups
+#'
+#' @name interestingGroups
+#'
+#' @inheritParams general
+#'
+#' @return Character vector.
+#'
+#' @examples
+#' \dontrun{
+#' load(system.file("extdata/se.rda", package = "bcbioBase"))
+#'
+#' # SummarizedExperiment
+#' interestingGroups(se)
+#'
+#' # Assignment support
+#' interestingGroups(se) <- "sampleID"
+#' interestingGroups(se)
+#' }
+NULL
+
+
+
+# Methods ======================================================================
+#' @rdname interestingGroups
+#' @export
+setMethod(
+    "interestingGroups",
+    signature("SummarizedExperiment"),
+    function(object) {
+        metadata(object)[["interestingGroups"]]
+    }
+)
+
+
+
+#' @rdname interestingGroups
+#' @export
+setMethod(
+    "interestingGroups<-",
+    signature(
+        object = "SummarizedExperiment",
+        value = "character"
+    ),
+    function(object, value) {
+        assertFormalInterestingGroups(
+            x = colData(object),
+            interestingGroups = value
+        )
+        metadata(object)[["interestingGroups"]] <- value
+        validObject(object)
+        object
+    }
+)
