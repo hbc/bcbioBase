5f288c95f8438c690a40d2b9c302fec6ff9e1cc7
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-14 07:03:48 -0400

    Deprecate `annotable()` in both bcbioRNASeq and bcbioSingleCell objects

--- R/deprecated.R
+++ R/deprecated.R
@@ -8,4 +8,25 @@
 #' @return No value.
 NULL
 
+
+
+# v0.2.0 =======================================================================
+# `annotable()` deprecated in basejump v0.4.0
+#' @rdname deprecated
+#' @importFrom SummarizedExperiment rowData
+#' @export
+setMethod(
+    "annotable",
+    signature("SummarizedExperiment"),
+    function(object) {
+        warn(paste(
+            "'annotable' is deprecated.",
+            "Use 'rowData' instead.",
+            "See help(\"Deprecated\")",
+            sep = "\n"
+        ))
+        rowData(object)
+    }
+)
+
 # nocov end
