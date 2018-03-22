babbf138c1895996b83d574e247f07829608781e
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-22 19:57:50 -0400

    Move assert checks

--- R/assert.R
+++ R/assert.R
@@ -9,3 +9,35 @@
 #'
 #' @return Silent on success, stop on failure.
 NULL
+
+
+
+#' @importFrom assertive assert_all_are_dirs
+#' @importFrom assertive assert_all_are_existing_files
+#' @importFrom assertive assert_all_are_in_range
+#' @importFrom assertive assert_all_are_non_missing_nor_empty_character
+#' @importFrom assertive assert_are_disjoint_sets
+#' @importFrom assertive assert_are_identical
+#' @importFrom assertive assert_are_intersecting_sets
+#' @importFrom assertive assert_has_colnames
+#' @importFrom assertive assert_has_dimnames
+#' @importFrom assertive assert_has_names
+#' @importFrom assertive assert_has_rownames
+#' @importFrom assertive assert_has_no_duplicates
+#' @importFrom assertive assert_is_a_bool
+#' @importFrom assertive assert_is_a_string
+#' @importFrom assertive assert_is_all_of
+#' @importFrom assertive assert_is_any_of
+#' @importFrom assertive assert_is_character
+#' @importFrom assertive assert_is_identical_to_na
+#' @importFrom assertive assert_is_integer
+#' @importFrom assertive assert_is_list
+#' @importFrom assertive assert_is_non_empty
+#' @importFrom assertive assert_is_subset
+#' @importFrom assertive assert_is_tbl
+#'
+#' @importFrom basejump assertIsAnImplicitInteger
+#' @importFrom basejump assertIsCharacterOrNULL
+#' @importFrom basejump assertIsGene2symbol
+#' @importFrom basejump assertIsAStringOrNULL
+NULL
