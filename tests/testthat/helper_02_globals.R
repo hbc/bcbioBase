a195eb8d8a4cf9ea0039d15c5f5defbde75cc603
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-24 18:30:13 -0400

    Add globals for heatmap unit tests

---
+++ tests/testthat/helper_02_globals.R
@@ -0,0 +1,17 @@
+mat <- matrix(
+    data = seq(1L:16L),
+    nrow = 4L,
+    ncol = 4L,
+    byrow = FALSE,
+    dimnames = list(
+        c(
+            "ENSG00000000001",
+            "ENSG00000000002",
+            "ENSG00000000003",
+            "ENSG00000000004"
+        ),
+        c("sample_1", "sample_2", "sample_3", "sample_4")
+    )
+)
+dgc <- as(mat, "dgCMatrix")
+heatmapList <- c("tree_row", "tree_col", "kmeans", "gtable")
