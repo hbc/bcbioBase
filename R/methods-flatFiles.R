d628a5823330f4208a966885039157417ba72eab
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-01 13:05:09 -0400

    Improve coercion method for RSE

--- R/methods-flatFiles.R
+++ R/methods-flatFiles.R
@@ -18,6 +18,12 @@ setMethod(
     "flatFiles",
     signature("SummarizedExperiment"),
     function(object) {
-        as(object, "list")
+        # Need to coerce to RSE prior to SE to keep rowRanges/rowData intact
+        if (is(object, "RangedSummarizedExperiment")) {
+            object <- as(object, "RangedSummarizedExperiment")
+        }
+        object <- as(object, "SummarizedExperiment")
+        list <- as(object, "list")
+        list
     }
 )
