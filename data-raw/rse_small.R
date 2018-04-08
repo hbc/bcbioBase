e50aee345afc789dff60dc33446e3b7cd2e0e961
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-08 17:27:45 -0400

    Make rse_small using `makeExampleDESeqDataSet()`. This is more minimal and tests our gene2symbol code handling

--- data-raw/rse_small.R
+++ data-raw/rse_small.R
@@ -1,10 +1,21 @@
 library(devtools)
 library(pryr)
-library(bcbioRNASeq)
-rse_small <- as(
-    object = bcbioRNASeq::bcb_small,
-    Class = "RangedSummarizedExperiment"
-)
-assays(rse_small) <- assays(rse_small)[1L]
+library(DESeq2)
+
+# bcbioRNASeq coercion
+# rse_small <- as(
+#     object = bcbioRNASeq::bcb_small,
+#     Class = "RangedSummarizedExperiment"
+# )
+# assays(rse_small) <- assays(rse_small)[1L]
+
+# DESeqDataSet coercion
+dds_small <- makeExampleDESeqDataSet()
+rse_small <- as(dds_small, "RangedSummarizedExperiment")
+
+stopifnot(identical(
+    names(assays(rse_small)),
+    "counts"
+))
 object_size(rse_small)
 use_data(rse_small, compress = "xz", overwrite = TRUE)
