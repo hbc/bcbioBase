346753b10598aa81d2d7a889c83a58184cf17db4
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-12 13:24:46 -0400

    Consolidate assert check documentation

--- R/assert.R
+++ R/assert.R
@@ -1,3 +1,17 @@
+#' Assert Checks
+#'
+#' @name assert
+#' @keywords internal
+#'
+#' @inheritParams general
+#' @param severity How severe should the consequences of the assertion be?
+#'   Either "`stop`", "`warning`", "`message`", or "`none`".
+#'
+#' @return Silent on success, stop on failure.
+NULL
+
+
+
 #' Interesting Groups Formal Assert Check
 #'
 #' Prevent unwanted downstream behavior when a missing interesting group
