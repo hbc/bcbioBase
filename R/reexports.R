dd1fe527ef41b9e14e05b052f82e028bf45a105b
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-05 15:11:12 -0400

    Update reexports

--- R/reexports.R
+++ R/reexports.R
@@ -14,6 +14,14 @@ GenomicRanges::GRanges
 #' @export
 ggplot2::ggplot
 
+#' @importFrom ggplot2 scale_color_gradient
+#' @export
+ggplot2::scale_color_gradient
+
+#' @importFrom ggplot2 scale_color_gradientn
+#' @export
+ggplot2::scale_color_gradientn
+
 #' @importFrom ggplot2 scale_color_hue
 #' @export
 ggplot2::scale_color_hue
@@ -21,6 +29,22 @@ ggplot2::scale_color_hue
 #' @importFrom ggplot2 scale_color_manual
 #' @export
 ggplot2::scale_color_manual
+
+#' @importFrom ggplot2 scale_color_viridis_c
+#' @export
+ggplot2::scale_color_viridis_c
+
+#' @importFrom ggplot2 scale_color_viridis_d
+#' @export
+ggplot2::scale_color_viridis_d
+
+#' @importFrom ggplot2 scale_colour_gradient
+#' @export
+ggplot2::scale_colour_gradient
+
+#' @importFrom ggplot2 scale_colour_gradientn
+#' @export
+ggplot2::scale_colour_gradientn
 
 #' @importFrom ggplot2 scale_colour_hue
 #' @export
@@ -30,6 +54,14 @@ ggplot2::scale_colour_hue
 #' @export
 ggplot2::scale_colour_manual
 
+#' @importFrom ggplot2 scale_colour_viridis_c
+#' @export
+ggplot2::scale_colour_viridis_c
+
+#' @importFrom ggplot2 scale_colour_viridis_d
+#' @export
+ggplot2::scale_colour_viridis_d
+
 #' @importFrom ggplot2 scale_fill_hue
 #' @export
 ggplot2::scale_fill_hue
@@ -37,6 +69,14 @@ ggplot2::scale_fill_hue
 #' @importFrom ggplot2 scale_fill_manual
 #' @export
 ggplot2::scale_fill_manual
+
+#' @importFrom ggplot2 scale_fill_viridis_c
+#' @export
+ggplot2::scale_fill_viridis_c
+
+#' @importFrom ggplot2 scale_fill_viridis_d
+#' @export
+ggplot2::scale_fill_viridis_d
 
 
 
@@ -83,6 +123,18 @@ viridis::magma
 #' @importFrom viridis plasma
 #' @export
 viridis::plasma
+
+#' @importFrom viridis scale_color_viridis
+#' @export
+viridis::scale_color_viridis
+
+#' @importFrom viridis scale_colour_viridis
+#' @export
+viridis::scale_colour_viridis
+
+#' @importFrom viridis scale_fill_viridis
+#' @export
+viridis::scale_fill_viridis
 
 #' @importFrom viridis viridis
 #' @export
