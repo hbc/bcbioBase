fd6f8110b8fdb6b4a49ca64df1efcdd0f730f653
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-12 12:20:27 -0400

    Update documentation

--- R/general.R
+++ R/general.R
@@ -8,7 +8,7 @@
 #' @param x Object.
 #' @param ... Additional arguments.
 #'
-#' @param file File path.
+#' @param file `string`. File path.
 #' @param interestingGroups `character`. Groups of interest.
 #' @param return Object class to return. Uses [match.arg()] internally and picks
 #'   the first item in the vector by default.
