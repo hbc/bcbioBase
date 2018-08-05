d4e90c61d591596e942dc61cd63c02b0ce9f718a
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-05 07:56:12 -0400

    Simplify package NAMESPACE

--- R/bcbioBase-package.R
+++ R/bcbioBase-package.R
@@ -2,61 +2,35 @@
 #'
 #' Base functions and generics for bcbio R packages.
 #'
-#' @importClassesFrom SummarizedExperiment SummarizedExperiment
-#'
-#' @importFrom assertive.base assert_are_identical assert_is_identical_to_na
-#' @importFrom assertive.data assert_all_are_hex_colors
+#' @importFrom assertive.base assert_are_identical
 #' @importFrom assertive.files assert_all_are_dirs assert_all_are_existing_files
-#' @importFrom assertive.numbers assert_all_are_greater_than
-#'   assert_all_are_in_range assert_all_are_non_negative assert_all_are_positive
-#' @importFrom assertive.properties assert_has_colnames assert_has_dimnames
-#'   assert_has_dims assert_has_names assert_has_no_duplicates
-#'   assert_has_rownames assert_is_atomic assert_is_non_empty has_dims
-#' @importFrom assertive.sets assert_are_disjoint_sets
-#'   assert_are_intersecting_sets assert_is_subset
+#' @importFrom assertive.numbers assert_all_are_in_range assert_all_are_positive
+#' @importFrom assertive.properties assert_has_dimnames assert_has_no_duplicates
+#'   assert_is_non_empty
+#' @importFrom assertive.sets assert_are_disjoint_sets assert_is_subset
 #' @importFrom assertive.strings assert_all_are_matching_regex
 #'   assert_all_are_non_missing_nor_empty_character
-#' @importFrom assertive.types assert_is_a_bool assert_is_a_number
-#'   assert_is_a_string assert_is_all_of assert_is_an_integer assert_is_any_of
-#'   assert_is_character assert_is_factor assert_is_function assert_is_integer
-#'   assert_is_list assert_is_matrix assert_is_tbl is_a_string
-#' @importFrom basejump assertFormalInterestingGroups assertHasRownames
-#'   assertIsAnImplicitInteger assertIsAStringOrNULL
-#'   assertIsHexColorFunctionOrNULL assertIsTx2gene camel emptyRanges fixNA
-#'   localOrRemoteFile makeNames printString readFileByExtension readYAML
-#'   removeNA snake
+#' @importFrom assertive.types assert_is_a_string assert_is_an_integer
+#'   assert_is_any_of assert_is_character assert_is_function assert_is_list
+#' @importFrom basejump assertIsAnImplicitInteger assertIsTx2gene camel fixNA
+#'   localOrRemoteFile makeNames readFileByExtension readYAML removeNA
 #' @importFrom Biostrings reverseComplement
-#' @importFrom dendsort dendsort
 #' @importFrom dplyr arrange everything funs group_by left_join mutate
-#'   mutate_all mutate_at mutate_if select select_if ungroup
-#' @importFrom GenomeInfoDb seqnames
-#' @importFrom GenomicRanges GRanges
+#'   mutate_all mutate_at mutate_if select ungroup
 #' @importFrom ggplot2 aes geom_hline geom_label geom_vline
 #' @importFrom ggrepel geom_label_repel
-#' @importFrom grDevices colorRampPalette
 #' @importFrom grid arrow unit
-#' @importFrom knitr kable
-#' @importFrom magrittr %>% set_names set_rownames
-#' @importFrom methods .hasSlot as formalArgs getMethod is setAs slotNames
-#'   validObject
-#' @importFrom pheatmap pheatmap
+#' @importFrom magrittr %>% set_rownames
+#' @importFrom methods as is
 #' @importFrom plyr ldply
-#' @importFrom RColorBrewer brewer.pal
 #' @importFrom rdrop2 drop_acc drop_auth drop_create drop_delete drop_exists
 #'   drop_get_metadata drop_share drop_upload
 #' @importFrom readr read_csv read_lines
 #' @importFrom rlang !!! !! sym syms
-#' @importFrom S4Vectors aggregate as.data.frame cor mcols mcols<- merge
-#'   metadata<-
-#' @importFrom scales percent
-#' @importFrom sessioninfo session_info
-#' @importFrom stats as.formula hclust quantile
+#' @importFrom S4Vectors aggregate as.data.frame merge
+#' @importFrom stats as.formula
 #' @importFrom stringr str_pad str_trunc
-#' @importFrom SummarizedExperiment assay assays colData colData<- rowData
-#'   SummarizedExperiment
-#' @importFrom tibble as_tibble column_to_rownames has_rownames is_tibble
-#'   rownames_to_column tibble
+#' @importFrom tibble column_to_rownames rownames_to_column tibble
 #' @importFrom tidyr expand
-#' @importFrom utils globalVariables sessionInfo
-#' @importFrom viridis viridis
+#' @importFrom utils globalVariables
 "_PACKAGE"
--- R/reexports.R
+++ R/reexports.R
@@ -4,113 +4,15 @@ dplyr::pull
 
 
 
-#' @importFrom GenomicRanges GRanges
-#' @export
-GenomicRanges::GRanges
-
-
-
-#' @importFrom grDevices colorRampPalette
-#' @export
-grDevices::colorRampPalette
-
-
-
 #' @importFrom ggplot2 ggplot
 #' @export
 ggplot2::ggplot
-
-#' @importFrom ggplot2 scale_color_gradient
-#' @export
-ggplot2::scale_color_gradient
-
-#' @importFrom ggplot2 scale_color_gradientn
-#' @export
-ggplot2::scale_color_gradientn
-
-#' @importFrom ggplot2 scale_color_hue
-#' @export
-ggplot2::scale_color_hue
-
-#' @importFrom ggplot2 scale_color_manual
-#' @export
-ggplot2::scale_color_manual
-
-#' @importFrom ggplot2 scale_color_viridis_c
-#' @export
-ggplot2::scale_color_viridis_c
-
-#' @importFrom ggplot2 scale_color_viridis_d
-#' @export
-ggplot2::scale_color_viridis_d
-
-#' @importFrom ggplot2 scale_colour_gradient
-#' @export
-ggplot2::scale_colour_gradient
-
-#' @importFrom ggplot2 scale_colour_gradientn
-#' @export
-ggplot2::scale_colour_gradientn
-
-#' @importFrom ggplot2 scale_colour_hue
-#' @export
-ggplot2::scale_colour_hue
-
-#' @importFrom ggplot2 scale_colour_manual
-#' @export
-ggplot2::scale_colour_manual
-
-#' @importFrom ggplot2 scale_colour_viridis_c
-#' @export
-ggplot2::scale_colour_viridis_c
-
-#' @importFrom ggplot2 scale_colour_viridis_d
-#' @export
-ggplot2::scale_colour_viridis_d
-
-#' @importFrom ggplot2 scale_fill_hue
-#' @export
-ggplot2::scale_fill_hue
-
-#' @importFrom ggplot2 scale_fill_manual
-#' @export
-ggplot2::scale_fill_manual
-
-#' @importFrom ggplot2 scale_fill_viridis_c
-#' @export
-ggplot2::scale_fill_viridis_c
-
-#' @importFrom ggplot2 scale_fill_viridis_d
-#' @export
-ggplot2::scale_fill_viridis_d
-
-
-
-#' @importFrom IRanges IRanges
-#' @export
-IRanges::IRanges
 
 
 
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
-
-
-
-#' @importFrom RColorBrewer brewer.pal
-#' @export
-RColorBrewer::brewer.pal
-
-
-
-#' @importFrom S4Vectors DataFrame
-#' @export
-S4Vectors::DataFrame
-
-#' @importFrom S4Vectors metadata
-#' @export
-S4Vectors::metadata
 
 
 
@@ -121,33 +23,3 @@ tibble::glimpse
 #' @importFrom tibble tibble
 #' @export
 tibble::tibble
-
-
-
-#' @importFrom viridis inferno
-#' @export
-viridis::inferno
-
-#' @importFrom viridis magma
-#' @export
-viridis::magma
-
-#' @importFrom viridis plasma
-#' @export
-viridis::plasma
-
-#' @importFrom viridis scale_color_viridis
-#' @export
-viridis::scale_color_viridis
-
-#' @importFrom viridis scale_colour_viridis
-#' @export
-viridis::scale_colour_viridis
-
-#' @importFrom viridis scale_fill_viridis
-#' @export
-viridis::scale_fill_viridis
-
-#' @importFrom viridis viridis
-#' @export
-viridis::viridis
