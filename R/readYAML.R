f7d30a9c613fc850e1d964063c80a67f2a0b1598
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-05 07:36:25 -0400

    Draft update...still needs simplification.

--- R/readYAML.R
+++ R/readYAML.R
@@ -27,13 +27,13 @@ NULL
     # Currently max 2 keys are supported (e.g. summary, metrics).
     assert_all_are_in_range(length(keys), lower = 1L, upper = 2L)
 
-    yaml <- suppressMessages(readYAML(file))
+    yaml <- readYAML(file)
     assert_are_identical(
         x = names(yaml),
         y = c("date", "upload", "bcbio_system", "samples")
     )
 
-    # Focus on the sample YAML data
+    # Focus on the sample YAML data.
     yaml <- yaml[["samples"]]
     assert_is_list(yaml)
     assert_is_non_empty(yaml)
@@ -52,72 +52,94 @@ NULL
     )
 
     # Check that nested keys are present and early return on failure.
+    # Return `NULL` here instead of stopping, so we can handle bcbio RNA-seq
+    # fast mode runs.
     if (
         length(keys) == 2L &&
         !keys[[2L]] %in% names(yaml[[1L]][[keys[[1L]]]])
     ) {
-        return(NULL)  # nocov
+        return(NULL)
     }
 
-    # Flat metadata at the top level of the YAML is easy to parse.
-    flat <- lapply(
+    # Top-level sample metadata in the YAML is relatively easy to parse.
+    # Just select for atomic values.
+    top <- sapply(
         X = yaml,
-        FUN = function(x) {
-            x[yamlFlatCols]
-        }
+        FUN = function(item) {
+            x <- sapply(
+                X = item,
+                FUN = function(item) {
+                    if (is.atomic(item)) {
+                        item
+                    } else {
+                        NULL
+                    }
+                },
+                simplify = TRUE,
+                USE.NAMES = TRUE
+            )
+            Filter(Negate(is.null), x)
+        },
+        simplify = TRUE,
+        USE.NAMES = FALSE
     )
-    # Coerce to tibble.
-    flat <- ldply(
-        .data = flat,
+    top_df <- as.data.frame(top)
+    dim(top_df)
+    top_DF <- as(top, "DataFrame")
+    dim(top_DF)
+    # Coerce to data.frame.
+    # TODO Make this a utility function in basejump.
+    top <- ldply(
+        .data = top,
         .fun = data.frame,
         stringsAsFactors = FALSE
     )
-    assert_is_data.frame(flat)
-    assert_is_non_empty(flat)
+    assert_is_data.frame(top)
+    assert_is_non_empty(top)
+    top <- as(top, "DataFrame")
 
     # Handle the nested metadata, defined by the keys.
     # This step is a little tricker but should work consistently.
-    nested <- lapply(yaml, function(x) {
-        x <- x[[keys]]
-        assert_is_non_empty(x)
+    nested <- lapply(yaml, function(item) {
+        item <- item[[keys]]
+        assert_is_non_empty(item)
         # Sanitize names into camel case here, otherwise they'll get modified
-        # during the `ldply()` call below.
-        x <- camel(x)
-        x <- lapply(x, function(x) {
-            if (length(x) > 1L) {
+        # during the `ldply()` call that coerces `list` to `data.frame`.
+        item <- camel(item)
+        item <- lapply(item, function(item) {
+            if (length(item) > 1L) {
                 # Detect and coerce nested metadata back to a string, if
                 # necessary. bcbio allows nesting with a semicolon delimiter.
-                # Warn the user here about discouraging with R data.
+                # Warn the user here about discouraging this with R data.
                 # http://bit.ly/2Je1xgO
                 warning("Nested sample metadata detected")
-                paste(x, collapse = "; ")
+                paste(item, collapse = "; ")
             } else {
-                x
+                item
             }
         })
         # Remove any `NULL` items.
-        Filter(Negate(is.null), x)
+        Filter(Negate(is.null), item)
     })
-
-    # FIXME Improve this step.
-    # There's a nested list that we need to handle better here.
-    stop()
-
     # Coerce to data frame.
     # Note that we're using `plyr::ldply()` here because it can coerce a list
     # with uneven lengths. However, it will sanitize column names, so make sure
     # we convert to camel case before running this step.
     nested <- ldply(
         .data = nested,
-        .fun = tibble,
+        .fun = data.frame,
         stringsAsFactors = FALSE
     )
     assert_is_data.frame(nested)
     assert_is_non_empty(nested)
+    nested <- as(nested, "DataFrame")
 
-    # Bind the flat and nested data frames, then return.
-    assert_are_disjoint_sets(colnames(flat), colnames(nested))
-    cbind(flat, nested) %>%
+    # Bind the top and nested data frames, coerce to tibble, and return.
+    assert_are_disjoint_sets(
+        x = colnames(top),
+        y = colnames(nested)
+    )
+    cbind(top, nested) %>%
         as("tbl_df") %>%
         sanitizeNA() %>%
         removeNA() %>%
