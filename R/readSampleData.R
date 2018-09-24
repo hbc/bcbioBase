0106e8e5187158b90abdfc69997b8c4c762085fa
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-24 07:04:45 -0400

    Simplify tibble coercion step

--- R/readSampleData.R
+++ R/readSampleData.R
@@ -77,8 +77,10 @@ readSampleData <- function(file, lanes = 1L) {
 
     # Works with local or remote files.
     # Ensure coercion to tibble here, for consistent handling.
-    data <- import(file) %>%
-        as("tbl_df") %>%
+    data <- import(file)
+    assertHasRownames(data)
+    data <- data %>%
+        as_tibble(rownames = "rowname") %>%
         camel() %>%
         removeNA()
 
