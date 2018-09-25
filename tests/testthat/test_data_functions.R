24b487348e44d12966e253e37c1af94510166e47
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-25 12:52:11 -0400

    Update unit tests

--- tests/testthat/test_data_functions.R
+++ tests/testthat/test_data_functions.R
@@ -4,20 +4,6 @@ uploadDir <- system.file("extdata/bcbio", package = "bcbioBase")
 
 
 
-# minimalSampleData ============================================================
-test_that("minimalSampleData", {
-    expect_identical(
-        object = minimalSampleData(c("sample 1", "sample 2")),
-        expected = DataFrame(
-            sampleName = factor(c("sample 1", "sample 2")),
-            description = factor(c("sample 1", "sample 2")),
-            row.names = factor(c("sample_1", "sample_2"))
-        )
-    )
-})
-
-
-
 # projectDir ===================================================================
 test_that("projectDir", {
     expect_identical(
