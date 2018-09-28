12485477d955fa4153e7b173c9780aa879331a26
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-28 14:13:14 -0400

    Improve assert checks

--- R/runDate.R
+++ R/runDate.R
@@ -11,6 +11,9 @@
 #' @examples
 #' runDate("2018-01-01_illumina_rnaseq")
 runDate <- function(projectDir) {
+    assert_is_a_string(projectDir)
+    projectDir <- basename(projectDir)
+    stopifnot(grepl(projectDirPattern, projectDir))
     match <- str_match(
         string = basename(projectDir),
         pattern = projectDirPattern
