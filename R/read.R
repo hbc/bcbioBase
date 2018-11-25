8451a555c4f5fae2216c8169fc0da7927972e89f
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-25 15:04:04 -0500

    Improve consistency in sample data handling with other packages that import basejump

--- R/read.R
+++ R/read.R
@@ -132,8 +132,6 @@ readProgramVersions <- function(file) {
 #' @return `DataFrame`.
 #'
 #' @examples
-#' import <- basejump::import
-#'
 #' ## Demultiplexed
 #' file <- file.path(bcbioBaseCacheURL, "demultiplexed.csv")
 #' import(file)
@@ -164,7 +162,7 @@ readSampleData <- function(file, lanes = 0L) {
         removeNA()
 
     # Stop on input of blacklisted columns.
-    intersect <- intersect(.sampleDataBlacklist, colnames(data))
+    intersect <- intersect(colnames(data), metadataBlacklist)
     if (has_length(intersect)) {
         stop(paste0(
             paste("Invalid columns:", toString(intersect)), "\n",
@@ -276,47 +274,17 @@ readSampleData <- function(file, lanes = 0L) {
         }
     }
 
-    .returnSampleData(data)
+    .makeSampleData(data)
 }
 
 
 
-# Consistent sanitization for YAML and external file.
-# TODO Consolidate this code with `basejump::makeSampleData()`.
-.returnSampleData <- function(data) {
-    assert_is_tbl_df(data)
-    assert_is_subset("description", colnames(data))
-    assert_are_disjoint_sets(.sampleDataBlacklist, colnames(data))
-
-    # Set sampleName from description, if necessary
-    if (!"sampleName" %in% colnames(data)) {
-        data[["sampleName"]] <- data[["description"]]
-    }
-
-    # Ensure `sampleID` has valid names here. This allows for input of samples
-    # beginning with numbers or containing hyphens for example, which aren't
-    # valid names in R.
-    data <- data %>%
-        mutate_all(as.factor) %>%
-        mutate_all(droplevels) %>%
-        mutate(rowname = makeNames(!!sym("description"), unique = TRUE)) %>%
-        arrange(!!sym("rowname")) %>%
-        select(!!sym("sampleName"), everything()) %>%
-        as("DataFrame")
-
-    assertHasRownames(data)
-    data
+# Note that this code is shared with `getSampleDataFromYAML()`.
+.makeSampleData <- function(object) {
+    assert_is_subset("description", colnames(object))
+    assert_are_disjoint_sets(colnames(object), metadataBlacklist)
+    makeSampleData(object)
 }
-
-
-
-# Consider reworking to add "rowname" here.
-.sampleDataBlacklist <- c(
-    "filename",  # Note case. Use "fileName" instead.
-    "interestingGroups",
-    "samplename",  # Note case. Use "description" instead.
-    "sampleID"
-)
 
 
 
