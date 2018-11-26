0d3ef9ff824ac95b6a90fe52441c6ff4ecffeb2e
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-11-26 16:05:24 -0500

    Improve `uploadDir` param

--- R/params.R
+++ R/params.R
@@ -1,8 +1,12 @@
 #' Parameters
+#'
 #' @name params
 #' @keywords internal
+#'
 #' @param projectDir `string`. Project directory path.
-#' @param uploadDir `string`. File path to bcbio run upload directory.
+#' @param uploadDir `string`. Path to final upload directory. This path is set
+#'   when running "`bcbio_nextgen -w template`".
 #' @param yaml `list`. Project summary YAML.
+#'
 #' @return No value.
 NULL
