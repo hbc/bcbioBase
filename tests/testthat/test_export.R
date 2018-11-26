53a461e0a737ee2f6ecbfbdb92e86e5462bdfa35
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-25 20:27:35 -0500

    Update unit tests

---
+++ tests/testthat/test_export.R
@@ -0,0 +1,79 @@
+context("Export")
+
+
+
+# copyToDropbox ================================================================
+if (file.exists("token.rds")) {
+    files <- c("demultiplexed.csv", "multiplexed.csv")
+    dropboxDir <- file.path("bcbioBase_examples", "copyToDropbox")
+    test_that("copyToDropbox : RDS token enabled", {
+        object <- copyToDropbox(
+            files = files,
+            dir = dropboxDir,
+            rdsToken = "token.rds"
+        )
+        expect_is(object, "list")
+        expect_identical(
+            object = lapply(object[[1L]], class),
+            expected = list(
+                ".tag" = "character",
+                url = "character",
+                id = "character",
+                name = "character",
+                "path_lower" = "character",
+                "link_permissions" = "list",
+                "preview_type" = "character",
+                "client_modified" = "character",
+                "server_modified" = "character",
+                rev = "character",
+                size = "integer"
+            )
+        )
+    })
+
+    test_that("copyToDropbox : Shared Dropbox directory", {
+        expect_warning(
+            object = copyToDropbox(
+                files = files,
+                dir = paste0(dropboxDir, "_shared"),
+                rdsToken = "token.rds"
+            ),
+            regexp = "rdrop2 currently isn't working well with shared"
+        )
+        # Don't clean up directory, because we won't be able to check if shared.
+    })
+
+    test_that("copyToDropbox : Invalid parameters", {
+        expect_error(
+            object = copyToDropbox(files = NULL, dir = "."),
+            regexp = paste(
+                "is2 :",
+                "files is not in any of the classes 'character', 'list'."
+            )
+        )
+        expect_error(
+            object = copyToDropbox(files = "XXX.csv.gz", dir = "."),
+            regexp = paste(
+                "is_existing_file :",
+                "Some or all of the files specified by files do not exist."
+            )
+        )
+        expect_error(
+            object = copyToDropbox(files = files, dir = NULL),
+            regexp = paste(
+                "is_a_string :",
+                "dir is not of class 'character'; it has class 'NULL'"
+            )
+        )
+        expect_error(
+            object = copyToDropbox(
+                files = files,
+                dir = dropboxDir, rdsToken = "XXX.rds"
+            ),
+            regexp = paste(
+                "is_existing_file :",
+                "Some or all of the files specified by rdsToken do not exist."
+            )
+        )
+    })
+}
--- tests/testthat/test_import.R
+++ tests/testthat/test_import.R
@@ -1,89 +1,11 @@
-context("Import/Export")
-
-
-
-# copyToDropbox ================================================================
-if (file.exists("token.rds")) {
-    files <- c("demultiplexed.csv", "multiplexed.csv")
-    dropboxDir <- file.path("bcbioBase_examples", "copyToDropbox")
-    test_that("copyToDropbox : RDS token enabled", {
-        object <- copyToDropbox(
-            files = files,
-            dir = dropboxDir,
-            rdsToken = "token.rds"
-        )
-        expect_is(object, "list")
-        expect_identical(
-            object = lapply(object[[1L]], class),
-            expected = list(
-                ".tag" = "character",
-                url = "character",
-                id = "character",
-                name = "character",
-                "path_lower" = "character",
-                "link_permissions" = "list",
-                "preview_type" = "character",
-                "client_modified" = "character",
-                "server_modified" = "character",
-                rev = "character",
-                size = "integer"
-            )
-        )
-    })
-
-    test_that("copyToDropbox : Shared Dropbox directory", {
-        expect_warning(
-            object = copyToDropbox(
-                files = files,
-                dir = paste0(dropboxDir, "_shared"),
-                rdsToken = "token.rds"
-            ),
-            regexp = "rdrop2 currently isn't working well with shared"
-        )
-        # Don't clean up directory, because we won't be able to check if shared.
-    })
-
-    test_that("copyToDropbox : Invalid parameters", {
-        expect_error(
-            object = copyToDropbox(files = NULL, dir = "."),
-            regexp = paste(
-                "is2 :",
-                "files is not in any of the classes 'character', 'list'."
-            )
-        )
-        expect_error(
-            object = copyToDropbox(files = "XXX.csv.gz", dir = "."),
-            regexp = paste(
-                "is_existing_file :",
-                "Some or all of the files specified by files do not exist."
-            )
-        )
-        expect_error(
-            object = copyToDropbox(files = files, dir = NULL),
-            regexp = paste(
-                "is_a_string :",
-                "dir is not of class 'character'; it has class 'NULL'"
-            )
-        )
-        expect_error(
-            object = copyToDropbox(
-                files = files,
-                dir = dropboxDir, rdsToken = "XXX.rds"
-            ),
-            regexp = paste(
-                "is_existing_file :",
-                "Some or all of the files specified by rdsToken do not exist."
-            )
-        )
-    })
-}
+context("Import")
 
 
 
 # readDataVersions =============================================================
 test_that("readDataVersions", {
     x <- readDataVersions("data_versions.csv")
-    expect_is(x, "data.frame")
+    expect_is(x, "DataFrame")
     expect_identical(
         object = colnames(x),
         expected = c("genome", "resource", "version")
@@ -154,9 +76,7 @@ test_that("readSampleData : Demultiplexed", {
             paste0("sample2_L00", seq_len(4L))
         )
     )
-})
 
-test_that("readSampleData : Demultiplexed : Invalid", {
     # Required column check failure.
     expect_error(
         object = readSampleData("demultiplexed_invalid_missing.csv"),
@@ -170,7 +90,7 @@ test_that("readSampleData : Demultiplexed : Invalid", {
     )
 })
 
-test_that("readSampleData : Multiplexed : bcbioSingleCell", {
+test_that("readSampleData : Multiplexed", {
     file <- "multiplexed_indrops.csv"
 
     # Note that we're expecting this to sort by the rownames (`description`),
@@ -178,28 +98,16 @@ test_that("readSampleData : Multiplexed : bcbioSingleCell", {
     expect_identical(
         object = readSampleData(file),
         expected = DataFrame(
-            sampleName = factor(
-                c(
-                    "sample2_1",
-                    "sample1_1",
-                    "sample3_1",
-                    "sample4_1",
-                    "sample2_2",
-                    "sample1_2",
-                    "sample3_2",
-                    "sample4_2"
-                ),
-                levels = c(
-                    "sample1_1",
-                    "sample1_2",
-                    "sample2_1",
-                    "sample2_2",
-                    "sample3_1",
-                    "sample3_2",
-                    "sample4_1",
-                    "sample4_2"
-                )
-            ),
+            sampleName = factor(c(
+                "sample1_1",
+                "sample2_1",
+                "sample3_1",
+                "sample4_1",
+                "sample1_2",
+                "sample2_2",
+                "sample3_2",
+                "sample4_2"
+            )),
             fileName = factor(c(
                 rep("indrops1_R1.fastq.gz", times = 4L),
                 rep("indrops2_R1.fastq.gz", times = 4L)
@@ -207,69 +115,48 @@ test_that("readSampleData : Multiplexed : bcbioSingleCell", {
             # Valid rownames (sampleIDs) are generated from this column.
             # Note that we're sorting the sample metadata by this column.
             description = factor(c(
-                "indrops1-AGAGGATA",
                 "indrops1-ATAGAGAG",
+                "indrops1-AGAGGATA",
                 "indrops1-CTCCTTAC",
                 "indrops1-TATGCAGT",
-                "indrops2-AGAGGATA",
                 "indrops2-ATAGAGAG",
+                "indrops2-AGAGGATA",
                 "indrops2-CTCCTTAC",
                 "indrops2-TATGCAGT"
             )),
-            index = factor(
-                c(2L, 1L, 3L, 4L, 2L, 1L, 3L, 4L),
-                levels = seq_len(4L)
-            ),
+            index = factor(rep(seq_len(4L), times = 2L)),
             sequence = factor(
-                c(
-                    "TATCCTCT",
+                rep(c(
                     "CTCTCTAT",
-                    "GTAAGGAG",
-                    "ACTGCATA",
                     "TATCCTCT",
-                    "CTCTCTAT",
                     "GTAAGGAG",
                     "ACTGCATA"
-                ),
-                levels = c("ACTGCATA", "CTCTCTAT", "GTAAGGAG", "TATCCTCT")
+                ), times = 2L)
             ),
             aggregate = factor(
-                paste0("sample", c(2L, 1L, 3L, 4L, 2L, 1L, 3L, 4L)),
+                paste0("sample", rep(seq_len(4L), times = 2L)),
                 levels = paste0("sample", seq_len(4L))
             ),
             genotype = factor(
-                c(
-                    "knockout",
-                    "wildtype",
-                    "wildtype",
-                    "knockout",
-                    "knockout",
-                    "wildtype",
-                    "wildtype",
-                    "knockout"
-                ),
-                levels = c("knockout", "wildtype")  # Note order here.
+                rep(c("wildtype", "knockout"), times = 4L),
+                # Note that the order should be alphabetical here.
+                levels = c("knockout", "wildtype")
             ),
             revcomp = factor(
-                c(
-                    "AGAGGATA",
+                rep(c(
                     "ATAGAGAG",
-                    "CTCCTTAC",
-                    "TATGCAGT",
                     "AGAGGATA",
-                    "ATAGAGAG",
                     "CTCCTTAC",
                     "TATGCAGT"
-                ),
-                levels = c("AGAGGATA", "ATAGAGAG", "CTCCTTAC", "TATGCAGT")
+                ), times = 2L)
             ),
             row.names = c(
-                "indrops1_AGAGGATA",
                 "indrops1_ATAGAGAG",
+                "indrops1_AGAGGATA",
                 "indrops1_CTCCTTAC",
                 "indrops1_TATGCAGT",
-                "indrops2_AGAGGATA",
                 "indrops2_ATAGAGAG",
+                "indrops2_AGAGGATA",
                 "indrops2_CTCCTTAC",
                 "indrops2_TATGCAGT"
             )
@@ -281,37 +168,37 @@ test_that("readSampleData : Multiplexed : bcbioSingleCell", {
     expect_identical(
         object = rownames(object),
         expected = c(
-            "indrops1_L001_AGAGGATA",
             "indrops1_L001_ATAGAGAG",
-            "indrops1_L001_CTCCTTAC",
-            "indrops1_L001_TATGCAGT",
-            "indrops1_L002_AGAGGATA",
             "indrops1_L002_ATAGAGAG",
-            "indrops1_L002_CTCCTTAC",
-            "indrops1_L002_TATGCAGT",
-            "indrops1_L003_AGAGGATA",
             "indrops1_L003_ATAGAGAG",
-            "indrops1_L003_CTCCTTAC",
-            "indrops1_L003_TATGCAGT",
-            "indrops1_L004_AGAGGATA",
             "indrops1_L004_ATAGAGAG",
-            "indrops1_L004_CTCCTTAC",
-            "indrops1_L004_TATGCAGT",
-            "indrops2_L001_AGAGGATA",
             "indrops2_L001_ATAGAGAG",
-            "indrops2_L001_CTCCTTAC",
-            "indrops2_L001_TATGCAGT",
-            "indrops2_L002_AGAGGATA",
             "indrops2_L002_ATAGAGAG",
-            "indrops2_L002_CTCCTTAC",
-            "indrops2_L002_TATGCAGT",
-            "indrops2_L003_AGAGGATA",
             "indrops2_L003_ATAGAGAG",
-            "indrops2_L003_CTCCTTAC",
-            "indrops2_L003_TATGCAGT",
-            "indrops2_L004_AGAGGATA",
             "indrops2_L004_ATAGAGAG",
+            "indrops1_L001_AGAGGATA",
+            "indrops1_L002_AGAGGATA",
+            "indrops1_L003_AGAGGATA",
+            "indrops1_L004_AGAGGATA",
+            "indrops2_L001_AGAGGATA",
+            "indrops2_L002_AGAGGATA",
+            "indrops2_L003_AGAGGATA",
+            "indrops2_L004_AGAGGATA",
+            "indrops1_L001_CTCCTTAC",
+            "indrops1_L002_CTCCTTAC",
+            "indrops1_L003_CTCCTTAC",
+            "indrops1_L004_CTCCTTAC",
+            "indrops2_L001_CTCCTTAC",
+            "indrops2_L002_CTCCTTAC",
+            "indrops2_L003_CTCCTTAC",
             "indrops2_L004_CTCCTTAC",
+            "indrops1_L001_TATGCAGT",
+            "indrops1_L002_TATGCAGT",
+            "indrops1_L003_TATGCAGT",
+            "indrops1_L004_TATGCAGT",
+            "indrops2_L001_TATGCAGT",
+            "indrops2_L002_TATGCAGT",
+            "indrops2_L003_TATGCAGT",
             "indrops2_L004_TATGCAGT"
         )
     )
