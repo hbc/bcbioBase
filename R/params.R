348a31f8d2b905c304840cafe58bebc3ff431a25
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-12 09:57:56 -0500

    Improve documentation [ci skip]

--- R/params.R
+++ R/params.R
@@ -3,9 +3,9 @@
 #' @name params
 #' @keywords internal
 #'
-#' @param projectDir `string`. Project directory path.
-#' @param uploadDir `string`. Path to final upload directory. This path is set
-#'   when running "`bcbio_nextgen -w template`".
+#' @param projectDir `character(1)`. Project directory path.
+#' @param uploadDir `character(1)`. Path to final upload directory. This path is
+#'   set when running "`bcbio_nextgen -w template`".
 #' @param yaml `list`. Project summary YAML.
 #'
 #' @return No value.
