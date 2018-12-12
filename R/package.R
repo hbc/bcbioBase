92cb0d9c1effdc5676d4cb3703d05f59bf3bf645
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-12-12 09:04:08 -0500

    Simplify the NAMESPACE, relying on goalie for assertions

--- R/package.R
+++ R/package.R
@@ -9,20 +9,6 @@
 #'
 #' @importFrom Biostrings reverseComplement
 #' @importFrom S4Vectors DataFrame metadata<- na.omit tail
-#' @importFrom assertive.base assert_are_identical
-#' @importFrom assertive.files assert_all_are_dirs assert_all_are_existing_files
-#' @importFrom assertive.numbers assert_all_are_in_range
-#'   assert_all_are_non_negative
-#' @importFrom assertive.properties assert_has_no_duplicates assert_is_atomic
-#'   assert_is_non_empty assert_is_scalar
-#' @importFrom assertive.sets assert_are_disjoint_sets assert_is_subset
-#' @importFrom assertive.strings assert_all_are_matching_regex
-#'   assert_all_are_non_missing_nor_empty_character
-#'   assert_any_are_matching_regex
-#' @importFrom assertive.types assert_is_a_string assert_is_all_of
-#'   assert_is_an_integer assert_is_any_of assert_is_character assert_is_list
-#'   assert_is_matrix assert_is_tbl_df
-#' @importFrom assertthat assert_that
 #' @importFrom basejump Tx2Gene camel localOrRemoteFile makeNames makeSampleData
 #'   printString realpath removeNA sanitizeNA
 #' @importFrom dplyr arrange everything funs group_by left_join mutate
