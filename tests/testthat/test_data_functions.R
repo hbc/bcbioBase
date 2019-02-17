79c0b36ea6da9b4558c29595bcc564005e93b5e8
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-17 08:42:13 -0500

    Remove `minimalSampleData()` unit test, which is covered in basejump

--- tests/testthat/test_data_functions.R
+++ tests/testthat/test_data_functions.R
@@ -2,21 +2,6 @@ context("Data Functions")
 
 
 
-# minimalSampleData ============================================================
-test_that("minimalSampleData", {
-    expect_identical(
-        minimalSampleData(c("sample 1", "sample 2")),
-        data.frame(
-            sampleName = c("sample 1", "sample 2"),
-            description = c("sample 1", "sample 2"),
-            row.names = c("sample_1", "sample_2"),
-            stringsAsFactors = TRUE
-        )
-    )
-})
-
-
-
 # sampleDirs ===================================================================
 test_that("sampleDirs", {
     uploadDir <- system.file("extdata/bcbio", package = "bcbioBase")
