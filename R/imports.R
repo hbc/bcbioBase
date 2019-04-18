ca4b64c61ed1b136ad545c9f30811dc6737dbc1e
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-18 09:33:04 -0400

    Remove `funs()` from imports

--- R/imports.R
+++ R/imports.R
@@ -5,8 +5,8 @@
 #' @importFrom S4Vectors DataFrame metadata<- na.omit tail
 #' @importFrom basejump Tx2Gene camel import localOrRemoteFile makeNames
 #'   makeSampleData printString realpath removeNA sanitizeNA
-#' @importFrom dplyr arrange everything funs group_by left_join mutate
-#'   mutate_all mutate_at mutate_if select ungroup
+#' @importFrom dplyr arrange everything group_by left_join mutate mutate_all
+#'   mutate_at mutate_if select ungroup
 #' @importFrom goalie allAreAtomic allAreFiles allAreMatchingRegex
 #'   areDisjointSets assert containsAURL hasNoDuplicates hasRownames
 #'   isADirectory isAFile isCharacter isFile isMatchingRegex isInRange isInt
