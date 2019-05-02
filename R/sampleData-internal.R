c587d8c817880ffc0044cb32a5f0bde600e97be6
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-02 11:19:52 -0400

    Don't check for validNames here

--- R/sampleData-internal.R
+++ R/sampleData-internal.R
@@ -27,19 +27,23 @@
 
 .makeSampleData <- function(object) {
     object <- as(object, "DataFrame")
+
     # Note that we want to call `.assertIsSampleData()` earlier, so we can
     # detect if the user is attempting to input automatic columns, such as
     # "revcomp". At this point, automatic columsns are allowed, so we don't want
     # to check for them again here.
     assert(isSubset("description", colnames(object)))
-    # Set sampleName from description, if necessary.
+
+    # Set `sampleName` from `description`, if necessary.
     if (!"sampleName" %in% colnames(object)) {
         object[["sampleName"]] <- object[["description"]]
     }
+
     # Set the sample IDs as rownames, using the "description" column. Here we
     # are ensuring that the names are valid in R. This allows for input of
     # samples beginning with numbers or containing hyphens for example, which
     # aren't valid names in R. Note that periods and underscores are valid.
     rownames(object) <- makeNames(object[["description"]], unique = TRUE)
+
     makeSampleData(object)
 }
