4832c6790e1c6bf915ca4d65f22409771b9b5a5e
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-13 12:44:30 -0400

    Simplify imports

--- R/imports.R
+++ R/imports.R
@@ -5,20 +5,17 @@
 #' @importFrom basejump camelCase import localOrRemoteFile makeNames
 #'   makeSampleData printString realpath removeNA sanitizeNA
 #' @importFrom dplyr arrange mutate mutate_all mutate_if
-#' @importFrom goalie allAreAtomic allAreFiles allAreMatchingRegex
-#'   areDisjointSets assert containsAURL false hasLength hasNoDuplicates
-#'   hasRownames isADirectory isAFile isAny isCharacter isFile isMatchingRegex
-#'   isInRange isInt isNonEmpty isNonNegative isScalar isString isSubset
-#'   validNames
+#' @importFrom goalie allAreAtomic allAreFiles areDisjointSets assert
+#'   hasRownames isADirectory isAFile isCharacter isMatchingRegex isInRange
+#'   isInt isNonEmpty isScalar isString isSubset validNames
 #' @importFrom magrittr %>% set_colnames
 #' @importFrom methods as is new
 #' @importFrom plyr ldply
 #' @importFrom rdrop2 drop_acc drop_auth drop_create drop_delete drop_exists
 #'   drop_get_metadata drop_share drop_upload
-#' @importFrom readr read_csv read_lines
+#' @importFrom readr read_csv
 #' @importFrom rlang !!! !! sym syms
-#' @importFrom stringr str_match str_pad str_replace str_trunc
+#' @importFrom stringr str_match str_replace
 #' @importFrom tibble as_tibble tibble
-#' @importFrom tidyr expand
 #' @importFrom utils globalVariables
 NULL
