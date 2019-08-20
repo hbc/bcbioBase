13e0af460cda3a1ba9f3210ed14f09a8ac4cf24b
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-20 11:19:09 -0400

    Tighten up the internal code

--- R/sampleDirs.R
+++ R/sampleDirs.R
@@ -1,7 +1,7 @@
 #' Sample directories
 #'
 #' @author Michael Steinbaugh
-#' @note Updated 2019-08-05.
+#' @note Updated 2019-08-20.
 #' @export
 #'
 #' @inheritParams acidroxygen::params
@@ -17,14 +17,11 @@
 sampleDirs <- function(uploadDir) {
     assert(isADirectory(uploadDir))
     uploadDir <- realpath(uploadDir)
-
     ## Get the subdirectories in the upload directory.
     dirs <- list.dirs(uploadDir, full.names = TRUE, recursive = FALSE)
-
     ## Detect and remove nested dated project directory.
     projectDir <- suppressMessages(projectDir(uploadDir))
     dirs <- setdiff(dirs, projectDir)
-
     ## Double check that we're nuking any remaining dated directories, in case
     ## bcbio has been run multiple times.
     isSample <- !grepl(
@@ -32,13 +29,10 @@ sampleDirs <- function(uploadDir) {
         x = basename(dirs)
     )
     dirs <- dirs[isSample]
-
     ## Ensure there are sample directories in the upload.
     assert(isNonEmpty(dirs))
-
     ## Use the directory basenames for vector names.
     basenames <- basename(dirs)
-
     ## Return sample directory basenames that are valid names in R.
     ## We're using these to define the `sampleID` column in our object metadata.
     ## Refer to `make.names()` for the valid name conventions in R.
@@ -64,19 +58,15 @@ sampleDirs <- function(uploadDir) {
             toString(invalid, width = 100L)
         ))
     }
-
     ## Our `makeNames` function coerces periods and dashes to underscores.
     basenames <- makeNames(basenames)
-
     ## Assign our valid names to the absolute file paths.
     names(dirs) <- basenames
-
     message(sprintf(
         fmt = "%d %s detected:\n%s",
         length(dirs),
-        ngettext(n = length(dirs), msg1 = "sample", msg2 = "samples"),s
+        ngettext(n = length(dirs), msg1 = "sample", msg2 = "samples"),
         printString(sort(names(dirs)))
     ))
-
     dirs
 }
