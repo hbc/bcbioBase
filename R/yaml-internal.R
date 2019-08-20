8f238dac799e604de3ddcc690ea40df99e2eb948
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-20 12:25:59 -0400

    Rework sample YAML parse to use DataFrame internally

--- R/yaml-internal.R
+++ R/yaml-internal.R
@@ -20,16 +20,9 @@
 
 
 
-## Updated 2019-07-23.
-.sanitizeNumericAsCharacter <- function(x) {
-    any(grepl(x = x, pattern = "^[0-9\\.]+$"))
-}
-
-
-
-## Currently parsing of a maximum of 2 key levels is supported
+## Currently parsing of a maximum of 2 key levels is supported.
 ## (e.g. summary > metrics).
-## Updated 2019-07-23.
+## Updated 2019-08-20.
 .sampleYAML <- function(yaml, keys) {
     assert(
         .isSummaryYAML(yaml),
@@ -37,7 +30,6 @@
         isInRange(length(keys), lower = 1L, upper = 2L),
         isSubset("samples", names(yaml))
     )
-
     ## Focus on the sample YAML data.
     yaml <- yaml[["samples"]]
     assert(
@@ -49,17 +41,15 @@
             y = names(yaml[[1L]])
         )
     )
-
     ## Check that nested keys are present and early return on failure. Return
     ## `NULL` here instead of stopping, so we can handle bcbio RNA-seq fast
     ## mode.
     if (
-        length(keys) == 2L &&
-        !keys[[2L]] %in% names(yaml[[1L]][[keys[[1L]]]])
+        hasLength(keys, n = 2L) &&
+        !isSubset(x = keys[[2L]], y = names(yaml[[1L]][[keys[[1L]]]]))
     ) {
         return(NULL)  # nocov
     }
-
     ## Top-level sample metadata in the YAML is relatively easy to parse. Just
     ## select for atomic values, otherwise return `NA`.
     top <- vapply(
@@ -67,7 +57,7 @@
         FUN = function(item) {
             ## Note that this will return variable length, so `vapply()`
             ## approach doesn't work here. Better method to use instead?
-            return <- sapply(
+            x <- sapply(
                 X = item,
                 FUN = function(item) {
                     if (is.atomic(item)) {
@@ -80,23 +70,20 @@
                 USE.NAMES = FALSE
             )
             ## Don't use `isCharacter()` assert here because we're allowing NA.
-            assert(is.character(return))
-            return
+            assert(is.character(x))
+            x
         },
         ## Require that there's no dimension mismatch when parsing.
         FUN.VALUE = character(length(yaml[[1L]])),
         USE.NAMES = TRUE
     )
     assert(is.matrix(top))
-    top <- top %>%
-        t() %>%
-        as_tibble() %>%
-        removeNA()
+    top <- t(top)
+    top <- as(top, "DataFrame")
     assert(
         identical(nrow(top), length(yaml)),
         allAreAtomic(top)
     )
-
     ## Handle the nested metadata, defined by the keys. This step is a little
     ## tricker but should work consistently.
     nested <- lapply(
@@ -127,33 +114,42 @@
             )
         }
     )
-    ## Use `ldply()` method to coerce a list with uneven lengths.
-    nested <- ldply(nested, data.frame, stringsAsFactors = FALSE) %>%
-        as_tibble() %>%
-        removeNA()
-
+    ## Using `ldply()` method here to coerce a list with uneven lengths.
+    nested <- ldply(
+        .data = nested,
+        .fun = data.frame,
+        stringsAsFactors = FALSE
+    )
+    nested <- as(nested, "DataFrame")
     assert(
         isNonEmpty(nested),
         allAreAtomic(nested),
         identical(nrow(top), nrow(nested)),
         areDisjointSets(colnames(top), colnames(nested))
     )
-
-    ## Bind the top and nested data frames, coerce to tibble, and return.
-    cbind(top, nested) %>%
-        as_tibble() %>%
-        camelCase() %>%
-        ## Coerce any periods in colnames to "x"
-        ## (e.g. x5.3Bias becomes x5x3Bias).
-        set_colnames(gsub("\\.", "x", colnames(.))) %>%
-        sanitizeNA() %>%
-        removeNA() %>%
-        arrange(!!sym("description")) %>%
-        ## Ensure numerics from YAML are set correctly and not character.
-        mutate_if(.sanitizeNumericAsCharacter, as.numeric) %>%
-        ## Order the columns alphabetically.
-        .[, sort(colnames(.)), drop = FALSE] %>%
-        ## Set the rownames.
-        mutate(rowname = makeNames(!!sym("description"), unique = TRUE)) %>%
-        as("DataFrame")
+    out <- cbind(top, nested)
+    out <- camelCase(out)
+    ## Coerce any periods in colnames to "x" (e.g. `x5.3Bias` to `x5x3Bias`).
+    colnames(out) <- gsub("\\.", "x", colnames(out))
+    out <- sanitizeNA(out)
+    out <- removeNA(out)
+    out <- out[order(out[["description"]]), , drop = FALSE]
+    ## Ensure numerics from YAML are set correctly and not character.
+    out <- DataFrame(lapply(
+        X = out,
+        FUN = function(x) {
+            if (
+                is.character(x) &&
+                any(grepl(x = x, pattern = "^[0-9\\.]+$"))
+            ) {
+                x <- as.numeric(x)
+            }
+            x
+        }
+    ))
+    ## Order the columns alphabetically.
+    out <- out[, sort(colnames(out)), drop = FALSE]
+    ## Set the rownames.
+    rownames(out) <- makeNames(out[["description"]], unique = TRUE)
+    out
 }
