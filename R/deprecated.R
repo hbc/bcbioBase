7ace6714c0f3371560aaae063b1d38bf4310d454
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-19 09:48:51 -0500

    roxygen documentation updates

--- R/deprecated.R
+++ R/deprecated.R
@@ -3,14 +3,6 @@
 
 
 
-#' @name defunct
-#' @inherit acidroxygen::defunct description examples return seealso title
-#' @inheritParams acidroxygen::params
-#' @keywords internal
-NULL
-
-
-
 #' @name deprecated
 #' @inherit acidroxygen::deprecated description examples return seealso title
 #' @inheritParams acidroxygen::params
