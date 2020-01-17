c62303d3bd83e1e5517ddbfc4810fb94efd7d16e
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-17 12:23:51 -0500

    Update imports

--- R/imports.R
+++ R/imports.R
@@ -4,7 +4,8 @@
 #' @importFrom S4Vectors DataFrame metadata<- na.omit tail
 #' @importFrom basejump camelCase import localOrRemoteFile makeNames
 #'   makeSampleData printString realpath removeNA sanitizeNA
-#' @importFrom cli cli_alert cli_alert_warning cli_dl cli_text cli_ul
+#' @importFrom cli cli_alert cli_alert_warning cli_dl cli_end cli_par cli_text
+#'   cli_ul
 #' @importFrom goalie allAreAtomic allAreFiles areDisjointSets
 #'   allAreMatchingRegex assert hasLength hasRownames isADirectory isAFile
 #'   isCharacter isMatchingRegex isInRange isInt isScalar isString isSubset
