de12e9d8a3d27c57dbc27a174d5b381b4c7c5593
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-10 09:36:11 -0500

    Improve YAML parser logic

--- R/internal-yaml.R
+++ R/internal-yaml.R
@@ -123,10 +123,7 @@
         FUN = function(x) {
             if (
                 is.character(x) &&
-                all(
-                    grepl(pattern = "^[0-9\\.]+$", x = x) ||
-                    is.na(x)
-                )
+                all(grepl(pattern = "^[0-9\\.]+$", x = x) | is.na(x))
             ) {
                 x <- as.numeric(x)
             } else if (is.character(x)) {
