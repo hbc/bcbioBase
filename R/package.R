bba34f8d66ee93099b3d173bba569e1a6ee93ab1
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-08 11:44:09 -0400

    Update NAMESPACE

--- R/package.R
+++ R/package.R
@@ -6,13 +6,13 @@
 #'
 #' @importMethodsFrom basejump coerce
 #'
-#' @importFrom basejump DataFrame abort alert alertInfo alertWarning as
-#'   camelCase dl import is localOrRemoteFile makeNames makeSampleData
-#'   metadata<- na.omit new packageName packageVersion printString
-#'   rbindToDataFrame realpath removeNA sanitizeNA str_match tail toInlineString
-#'   ul
+#' @importFrom basejump DataFrame abort alert alertInfo alertWarning camelCase
+#'   dl import localOrRemoteFile makeNames makeSampleData metadata<- na.omit
+#'   packageName packageVersion printString rbindToDataFrame realpath removeNA
+#'   sanitizeNA str_match tail toInlineString ul
 #' @importFrom goalie allAreAtomic allAreFiles areDisjointSets
 #'   allAreMatchingRegex assert hasLength hasRownames isADirectory isAFile
 #'   isCharacter isMatchingRegex isInRange isInt isScalar isString isSubset
 #'   validNames
+#' @importFrom methods as is new
 "_PACKAGE"
