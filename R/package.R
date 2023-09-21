e0d7cd7d0d938d630a210bad801fecce9885a9f9
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-21 12:49:01 -0400

    Remove stringr dependency

--- R/package.R
+++ R/package.R
@@ -14,20 +14,15 @@
 #' @importFrom S4Vectors metadata<- na.omit tail
 #' @importFrom pipette import removeNA sanitizeNA
 #' @importFrom syntactic camelCase makeNames
-#'
-#' @importMethodsFrom AcidExperiment makeSampleData
-#' @importMethodsFrom AcidPlyr rbindToDataFrame
-#' @importMethodsFrom pipette import removeNA sanitizeNA
-#' @importMethodsFrom syntactic camelCase makeNames
 NULL
 
 
 
 ## Standard functions ==========================================================
 
-#' @importFrom S4Vectors DataFrame
 #' @importFrom AcidBase printString realpath strMatch
 #' @importFrom AcidCLI abort alert alertInfo alertWarning dl toInlineString ul
+#' @importFrom S4Vectors DataFrame
 #' @importFrom goalie allAreAtomic allAreFiles areDisjointSets
 #' allAreMatchingRegex assert hasLength hasRownames isADirectory isAFile
 #' isCharacter isMatchingRegex isInRange isInt isScalar isString isSubset
